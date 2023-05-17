311c25f2805c535ded7b1e7ed7b9c3f71eb95745
rkpasupul <rpasupul@gmail.com>
2023-05-17 10:38:27 +0530

    the addmin feature is added to the projrct

--- my_tennis_club/my_tennis_club/settings.py
+++ my_tennis_club/my_tennis_club/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^fc@s=v7he+5t79mpul3l+jnc@d4*-qt*m7y^i%ym&bpw8_b_u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
